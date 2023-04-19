30b7be14fedfb0c1d3fdcf5148cac730890b9366
Canal do Front <canaldofront@gmail.com>
2023-04-19 17:03:02 -0300

    first commit

---
+++ scripts/index.js
@@ -0,0 +1,12 @@
+const navbar = document.querySelector('.navbar');
+const mobileNavbar = document.querySelector('.navbar__mobile');
+const button = document.querySelector('.burguer');
+
+button.addEventListener('click', function () {
+  mobileNavbar.classList.toggle('active');
+});
+
+window.addEventListener('scroll', function () {
+  if (this.window.pageYOffset > 0) return navbar.classList.add('active');
+  return navbar.classList.remove('active');
+});
